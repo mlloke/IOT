16cabdae3bc5493c216e7adf162733e4fc97f9bc
mlloke <loke.mee.ling@gmail.com>
2022-08-10 04:38:37 +0000

    class based API views

--- decoderapp/urls.py
+++ decoderapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
-    path('decoder/', views.decoder_list),
-    path('decoder/<int:pk>/', views.decoder_detail)
+    path('decoder/', views.decoder_list.as_view()),
+    path('decoder/<int:pk>/', views.decoder_detail.as_view()),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- decoderapp/views.py
+++ decoderapp/views.py
@@ -1,15 +1,60 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
+from django.http import Http404
+#from django.http import HttpResponse, JsonResponse
+#from rest_framework.parsers import JSONParser
+#from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from decoderapp.models import DeviceList
 from decoderapp.serializers import DeviceListSerializer
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
+
+"""
+tutorial 3
+"""
+
+class decoder_list(APIView):
+    def get(self, rquest, format=None):
+        dev = DeviceList.objects.all()
+        serializer = DeviceListSerializer(dev, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DeviceListSerializer(data=request.data)
+        if serializer.is_valid():
+            seralizer.save()
+            return Response(seralizer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class decoder_detail(APIView):
+    def get_object(self,pk):
+       try:
+           dev = DeviceList.objects.get(pk=pk)
+       except DeviceList.DoesNotExist:
+           raise Http404
+    
+    def get(self, request, pk, format=None):    
+        dev = self.get_object(pk)
+        serializer = DeviceListSerializer(dev)
+        return Response(serializer.data)
+        
+    def put(self, request, pk, format=None):
+        dev = self.get_object(pk)
+        serializer = DiviceListSerializer(dev, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk, format=None):    
+        dev = self.get_object(pk)
+        dev.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)        
+        
 
 """
 tutorial 2
-"""
+
 
 @api_view(['GET','POST'])
 def decoder_list(request):
@@ -47,7 +92,7 @@ def decoder_detail(request, pk):
     elif request.method == 'DELETE':
         dev.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+"""
 
 """
 tutorial 1
